b63a137f0119478190190b8b19ebf16ec0712493
Wojciech Bies <bies.wojciech@gmail.com>
2021-02-21 12:29:46 +0000

    introduction of property in Java

--- src/main/java/pl/sda/javalondek4springdemo/repository/BookRepository.java
+++ src/main/java/pl/sda/javalondek4springdemo/repository/BookRepository.java
@@ -2,11 +2,13 @@ package pl.sda.javalondek4springdemo.repository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import pl.sda.javalondek4springdemo.model.Book;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class BookRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(BookRepository.class);
@@ -21,5 +23,10 @@ public class BookRepository {
         this.books = new ArrayList<>(someBooks);
 
         logger.info("book repository initialized with books: {}", books);
+    }
+
+    public List<Book> findAllBooks() {
+
+        return books;
     }
 }
