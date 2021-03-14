802f9673e74dbd9cd515dd43bceb8ae509704d1f
Wojciech Bies <bies.wojciech@gmail.com>
2021-03-14 13:30:16 +0000

    [add] - SQL resources created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO BOOKS (IMIE, SURNAME, TYTUL)
+VALUES ('Henryk', 'Sienkiewicz', 'Pan Wolodyjowski');
+
+INSERT INTO BOOKS (IMIE, SURNAME, TYTUL)
+VALUES ('Henryk', 'Sienkiewicz', 'Potop');
+
+INSERT INTO BOOKS (IMIE, SURNAME, TYTUL)
+VALUES ('Henryk', 'Sienkiewicz', 'Ogniem i mieczem');
