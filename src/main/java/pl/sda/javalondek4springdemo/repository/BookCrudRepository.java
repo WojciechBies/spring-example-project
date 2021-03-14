ae388b11cd3372215a076fda20333dce591f0871
Wojciech Bies <bies.wojciech@gmail.com>
2021-03-14 13:00:12 +0000

    [change] - CrudRepository used as repository object

--- src/main/java/pl/sda/javalondek4springdemo/repository/BookCrudRepository.java
+++ src/main/java/pl/sda/javalondek4springdemo/repository/BookCrudRepository.java
@@ -1,9 +1,18 @@
 package pl.sda.javalondek4springdemo.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import pl.sda.javalondek4springdemo.model.Book;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
 public interface BookCrudRepository extends CrudRepository<Book, Long> {
 
+    @Query("from books")
+    List<Book> findAllBooks();
+
+    Optional<Book> findBookByTitle(String title);
 
 }
