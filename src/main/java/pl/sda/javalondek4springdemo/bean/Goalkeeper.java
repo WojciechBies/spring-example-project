b4e4d8ab5a4ba96dcd84792da81f59775706bfaf
Wojciech Bies <bies.wojciech@gmail.com>
2021-03-21 11:29:59 +0000

    [change] - goalkeeper fix

--- src/main/java/pl/sda/javalondek4springdemo/bean/Goalkeeper.java
+++ src/main/java/pl/sda/javalondek4springdemo/bean/Goalkeeper.java
@@ -3,16 +3,11 @@ package pl.sda.javalondek4springdemo.bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-<<<<<<< HEAD
-@Primary
-@Component("buffon")
-public class Goalkeeper implements Player {
-=======
+
 @Component("buffon")
 @Primary
 public class Goalkeeper implements Player {
 
->>>>>>> feature/relations-wb
     @Override
     public void play() {
         System.out.println("I'm goalkeeper");
